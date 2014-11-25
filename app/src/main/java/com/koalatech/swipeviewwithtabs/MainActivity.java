ff019b8773b322e83749748df784abe2a973f3f2
Richard M. Pabelona Jr <koala.linux@gmail.com>
2014-11-25 21:10:46 +0800

    Initial commit

---
+++ app/src/main/java/com/koalatech/swipeviewwithtabs/MainActivity.java
@@ -0,0 +1,82 @@
+package com.koalatech.swipeviewwithtabs;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
+    ActionBar actionbar;
+    FragmentPagerAdapter ft;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager)findViewById(R.id.pager);
+        ft = new ViewPagerAdapter(getSupportFragmentManager());
+        actionbar = getActionBar();
+        viewPager.setAdapter(ft);
+        actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionbar.addTab(actionbar.newTab().setText("Red Dog").setTabListener(this));
+        actionbar.addTab(actionbar.newTab().setText("Bluish Apple").setTabListener(this));
+        actionbar.addTab(actionbar.newTab().setText("Green Ice").setTabListener(this));
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                actionbar.setSelectedNavigationItem(i);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+    }
+
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+}
---
+++ app/src/main/java/com/koalatech/swipeviewwithtabs/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.koalatech.swipeviewwithtabs;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch(i){
+            case 0:
+                return new FragmentOne();
+            case 1:
+                return new FragmentTwo();
+            case 2:
+                return new FragmentThree();
+            default:
+                break;
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+}
